8a8497800468a3fd494c16d27cdf52471dc28988
hosung77 <ho4728@gmail.com>
2025-05-14 17:36:03 +0900

    first commit

---
+++ src/test/java/com/example/praticetokensecurity/PraticeTokenSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praticetokensecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeTokenSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
